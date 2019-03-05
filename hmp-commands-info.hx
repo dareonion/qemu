3966a1d9cfc79e97fa5fcd250585aa2675e7f75e
Darren Yin <darren.yin@gmail.com>
2019-03-05 11:35:46 -0800

    monitor: Add an "info pg" command that prints the current page tables
    The new "info pg" monitor command prints the current page table,
    including virtual address ranges, flag bits, and snippets of physical
    page numbers.  Completely filled regions of the page table with
    compatible flags are "folded", with the result that the complete
    output for a freshly booted x86-64 Linux VM can fit in a single
    terminal window.  The output looks like this:
    
    VPN range             Entry         Flags            Physical page
    [7f0000000-7f0000000] PML4[0fe]     ---DA--UWP
      [7f28c0000-7f28fffff]  PDP[0a3]     ---DA--UWP
        [7f28c4600-7f28c47ff]  PDE[023]     ---DA--UWP
          [7f28c4655-7f28c4656]  PTE[055-056] X--D---U-P 0000007f14-0000007f15
          [7f28c465b-7f28c465b]  PTE[05b]     ----A--U-P 0000001cfc
    ...
    [ff8000000-ff8000000] PML4[1ff]     ---DA--UWP
      [ffff80000-ffffbffff]  PDP[1fe]     ---DA---WP
        [ffff81000-ffff81dff]  PDE[008-00e] -GSDA---WP 0000001000-0000001dff
      [ffffc0000-fffffffff]  PDP[1ff]     ---DA--UWP
        [ffffff400-ffffff5ff]  PDE[1fa]     ---DA--UWP
          [ffffff5fb-ffffff5fc]  PTE[1fb-1fc] XG-DACT-WP 00000fec00 00000fee00
        [ffffff600-ffffff7ff]  PDE[1fb]     ---DA--UWP
          [ffffff600-ffffff600]  PTE[000]     -G-DA--U-P 0000001467
    
    Signed-off-by: Austin Clements <amdragon@mit.edu>

--- hmp-commands-info.hx
+++ hmp-commands-info.hx
@@ -251,6 +251,22 @@ STEXI
 Show the active virtual memory mappings.
 ETEXI
 
+#if defined(TARGET_I386)
+    {
+        .name       = "pg",
+        .args_type  = "",
+        .params     = "",
+        .help       = "show the page table",
+        .cmd        = pg_info,
+    },
+#endif
+
+STEXI
+@item info pg
+@findex info pg
+Show the page table.
+ETEXI
+
     {
         .name       = "mtree",
         .args_type  = "flatview:-f,dispatch_tree:-d,owner:-o",
